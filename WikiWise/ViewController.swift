9981f4487d2402c9806e17788a52472e5fa02614
maikytech <maikytech1307@gmail.com>
2020-04-09 17:15:38 -0500

    Start Project

---
+++ WikiWise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiWise
+//
+//  Created by Maiqui Cedeño on 9/04/20.
+//  Copyright © 2020 PosetoStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
